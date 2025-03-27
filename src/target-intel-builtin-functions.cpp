629cd3a021fa9d81312f1433423bb6b0c6fdcb27
Rafael Solano Martinez <nemesysolano@gmail.com>
2025-03-27 04:21:09 -0400

    commit Thu Mar 27 04:21:09 AM AST 2025

--- src/target-intel-builtin-functions.cpp
+++ src/target-intel-builtin-functions.cpp
@@ -1,8 +1,17 @@
 #include "target-intel-builtin-functions.h"
+#include "target-intel-instructions.h"
+#include "target-intel-registers.h"
+#include <ostream>
+using namespace std;
 
 void init_intel_builtin_functions(std::map<std::string, size_t> call_builtin_functions) {
 }
 
 void call_intel_sum(TargetContext & target_context, std::ostream & out, const std::map<std::string, size_t> & static_data, const std::map<std::string, size_t> & builtin_functions){
 
+}
+
+void clear_intel_trace_registers(std::ostream & out) {
+    out << '\t' << '\t' << NASM_SUB << ' ' << NASM_RAX << SEP << NASM_RAX << endl;
+    out << '\t' << '\t' << NASM_SUB << ' ' << NASM_RBX << SEP << NASM_RBX << endl;
 }
--- src/target-intel-linux.cpp
+++ src/target-intel-linux.cpp
@@ -72,66 +72,6 @@ void TargetIntelLinux::return_statement(TargetContext & target_context, std::ost
     out << '\t' << '\t' << NASM_JMP << ' ' << scope_name << EXIT_SUFFIX << endl;
 }
 
-/* *
-ExpressionResult TargetIntelLinux::evaluate_expression(TargetContext & target_context, ostream & out, const map<string, size_t> & static_data){     
-    auto const & object = * target_context.current();
-    auto const & object_name = object.getValue();
-    auto object_type = object.getType();
-    auto object_data_type = to_data_type(object_type);
-    auto object_data_type_size = data_type_size(object_data_type);
-    const char * size_qualifier = DWORD;
-    const char * size_register = NASM_EAX;
-    bool is_literal = false;    
-
-    out << '\t' << '\t' << NASM_SUB << ' ' << NASM_RAX << SEP << NASM_RAX << endl;
-    if(is_literal_token_type(object.getType())){
-        is_literal = true;
-        if(object_data_type == DataType::TEXT || object_data_type == DataType::FLOAT || object_data_type == DataType::BIGINT) {            
-            
-            switch(object_data_type) {
-                case DataType::TEXT:
-                    out << '\t' << '\t' << NASM_LEA << ' ' << NASM_RAX << SEP << STATIC_PREFIX << static_data.at(object.getValue()) << endl;                                        
-                    break;
-                    
-                case DataType::BIGINT:                    
-                    out << '\t' << '\t' << NASM_MOV << ' ' << NASM_RAX << SEP << object.getValue() << endl;
-                    break;
-
-                case DataType::FLOAT: // // mov     rax, qword [rel pi]  
-                    out << '\t' << '\t' << NASM_MOV << ' ' << NASM_RAX << SEP << QWORD << '[' << REL << ' ' << STATIC_PREFIX << static_data.at(object.getValue()) << ']' << endl;
-                    break;
-            }
-            
-        } else {
-            switch(object_data_type) {
-                case DataType::INTEGER:
-                    out << '\t' << '\t' << NASM_MOV << ' ' << NASM_EAX << SEP << object.getValue() << endl;                    
-                    break;
-
-                case DataType::BOOLEAN:
-                    out << '\t' << '\t' << NASM_MOV << ' ' <<  NASM_EAX << SEP << (parse_boolean(object.getValue()) ? 1 : 0) << endl;
-                    break;                
-            }
-        }
-    } else {        
-        auto object_offset = target_context.scopes.top()->objects.at(object_name)->offset;        
-        object_data_type = target_context.scopes.top()->objects.at(object_name)->data_type;
-        object_data_type_size = target_context.scopes.top()->objects.at(object_name)->size;
-
-        if(object_data_type_size == __SIZEOF_POINTER__) {
-            size_qualifier = QWORD;
-            size_register = NASM_RAX;
-        }
-
-        out << '\t' << '\t' << NASM_MOV << ' ' << size_register << SEP << '[' << NASM_RBP << '-' << object_offset <<  ']' << endl;
-        out << '\t' << '\t' << NASM_MOV << ' ' << size_qualifier << '[' <<  NASM_RBP << '-' << object_offset <<  ']' << SEP << size_register << endl;
-
-    }
-
-    return {is_literal, object_data_type, object_data_type_size};
-}
-/* */
-
 void TargetIntelLinux::print_statement(TargetContext & target_context, std::ostream & out, const std::map<std::string, size_t> & static_data) {   
     size_t argc = 0; 
     const auto & scope_name = target_context.scopes.top().get()->name;    
@@ -233,10 +173,12 @@ void TargetIntelLinux::call_statement(TargetContext & target_context, ostream & 
 
 void TargetIntelLinux::statements(TargetContext & target_context, ostream & out, const map<string, size_t> & static_data, const std::map<std::string, size_t> & builtin_functions) {
     assert(target_context.next()->getType() == TokenType::BEGIN);
-   
+    
+    clear_intel_trace_registers(out);
+
     while(target_context.current()->getType() != TokenType::END) {
 
-        switch(target_context.current()->getType()) {
+        switch(target_context.current()->getType()) {            
             case TokenType::RETURN:
                 return_statement(target_context, out, static_data);
                 break;
